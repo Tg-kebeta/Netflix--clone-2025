98cf099be5792641c70cbc0162982bae53538ab8
Tg-Kebeta <tigist.chefessa@gmail.com>
2025-09-09 21:45:55 -0400

    changes
    changes on the home folder

--- netflix_clone/src/App.jsx
+++ netflix_clone/src/App.jsx
@@ -4,17 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import "./App.css";
-import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Header/Header";
+import Home from './Pages/Home/Home';
+
 
 
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
-      
-      
+      <Home/>
     </>
   );
 }
