67db4126f4e0f3005b4c29fd9d5c8ff6b4bbb392
Tg-Kebeta <tigist.chefessa@gmail.com>
2025-09-11 23:18:13 -0400

    vite config update
    vite config change

--- netflix_clone/vite.config.js
+++ netflix_clone/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+base: "/Netflix--clone-2025/";
 export default defineConfig({
   plugins: [react()],
 })
