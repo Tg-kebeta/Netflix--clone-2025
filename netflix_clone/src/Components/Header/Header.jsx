4285cbc399670204604a40b1c7c2e8423ce725dd
Tg-Kebeta <tigist.chefessa@gmail.com>
2025-10-28 14:49:18 -0400

    responsive css

--- netflix_clone/src/Components/Header/Header.jsx
+++ netflix_clone/src/Components/Header/Header.jsx
@@ -1,27 +1,82 @@
 
-import React from 'react'
-import "./header.css"
+// import React from 'react'
+// import "./header.css"
+// import NetflixLogo from "../../assets/images/logo.png";
+// import SearchIcon from "@mui/icons-material/Search";
+// import NotificationsIcon from "@mui/icons-material/Notifications";
+// import AccountBoxIcon from "@mui/icons-material/AccountBox";
+// import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+// const Header = () => {
+//   return (
+//     <div>
+//       <div className="header-container">
+//         <div className="header-left">
+//           <img src={NetflixLogo} alt="Netflix Logo" className="netflix-logo" />
+//           <ul className="nav-links">
+//             <li>Home</li>
+//             <li>TV Shows</li>
+//             <li>Movies</li>
+//             <li>Games</li>
+//             <li>New & Popular</li>
+//             <li>Browse by Languages</li>
+//           </ul>
+//         </div>
+//         <div className="header-right">
+//           <SearchIcon className="icon" />
+//           <NotificationsIcon className="icon" />
+//           <AccountBoxIcon className="icon" />
+//           <ArrowDropDownIcon className="icon" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Header
+
+import React, { useState } from "react";
+import "./header.css";
 import NetflixLogo from "../../assets/images/logo.png";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div>
+    <div className="header-outer-container">
       <div className="header-container">
+        {/* LEFT SIDE */}
         <div className="header-left">
           <img src={NetflixLogo} alt="Netflix Logo" className="netflix-logo" />
-          <ul className="nav-links">
+
+          {/* Hamburger icon (only visible on small screens) */}
+          <div className="hamburger-icon" onClick={toggleMenu}>
+            {menuOpen ? <CloseIcon /> : <MenuIcon />}
+          </div>
+
+          {/* Nav Links */}
+          <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
             <li>Home</li>
             <li>TV Shows</li>
             <li>Movies</li>
             <li>Games</li>
             <li>New & Popular</li>
+            <li>My List</li>
             <li>Browse by Languages</li>
           </ul>
         </div>
+
+        {/* RIGHT SIDE */}
         <div className="header-right">
           <SearchIcon className="icon" />
           <NotificationsIcon className="icon" />
@@ -31,6 +86,6 @@ const Header = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
